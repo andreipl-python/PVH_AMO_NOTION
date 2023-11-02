ee636767a6f62871c979524eb9d0684152a0b9e2
Andrei <257141776a@gmail.com>
2023-11-02 13:18:55 +0300

    Notion class methods

--- api_models/notion_models.py
+++ api_models/notion_models.py
@@ -3,6 +3,7 @@ from pprint import pprint
 
 import aiohttp
 
+
 class Notion:
     def __init__(self):
         self.url = 'https://api.notion.com/v1/'
@@ -12,24 +13,124 @@ class Notion:
             "Content-Type": "application/json"
         }
 
-    async def get(self, method: str) -> dict:
+    async def __get(self, method: str) -> dict:
         async with aiohttp.ClientSession() as session:
             async with session.get(f'{self.url}{method}', headers=self.headers) as response:
                 return await response.json()
 
-    async def post(self, method: str, data: dict) -> dict:
+    async def __post(self, method: str, data: dict = None) -> dict:
         async with aiohttp.ClientSession() as session:
             async with session.post(f'{self.url}{method}', json=data, headers=self.headers) as response:
                 return await response.json()
 
-    async def get_users(self):
-        method = 'pages/0acf167a819e420da46425965e8de441'
-        return await self.get(method)
+    async def __patch(self, method: str, data: dict) -> dict:
+        async with aiohttp.ClientSession() as session:
+            async with session.patch(f'{self.url}{method}', json=data, headers=self.headers) as response:
+                return await response.json()
 
+    async def __delete(self, method: str) -> dict:
+        async with aiohttp.ClientSession() as session:
+            async with session.delete(f'{self.url}{method}', headers=self.headers) as response:
+                return await response.json()
+
+    async def get_database(self, db_id: str) -> dict:
+        method = f'databases/{db_id}'
+        return await self.__get(method)
+
+    async def database_query(self, db_id: str) -> dict:
+        method = f'databases/{db_id}/query'
+        return await self.__post(method=method)
+
+    async def insert_into_db(self, db_id: str) -> dict:
+        method = 'pages'
+        data = {
+            'parent': {
+                'type': 'database_id',
+                'database_id': db_id
+            },
+            'properties': {
+                'Name': {
+                    'title': [
+                        {
+                            'text': {
+                                'content': 'Ololotrololo'
+                            }
+                        }
+                    ]
+                },
+                'Number': {
+                    'number': 12541
+                },
+                'Text': {
+                    'multi_select': [
+                        {
+                            'name': 'text2'
+                        }
+                    ]
+                }
+            }
+        }
+        return await self.__post(method=method, data=data)
+
+    async def delete_block(self):
+        method = 'blocks/c6e6b21a-afac-454b-8b14-0d845fd4cf05'
+        return await self.__delete(method=method)
+
+    async def create_table(self):
+        """PAGE_ID"""
+        method = 'blocks/0acf167a819e420da46425965e8de441/children'
+        data = {
+            'children': [
+                {
+                    'table': {
+                        'table_width': 6,
+                        'has_column_header': True,
+                        'children': [
+                            {
+                                "table_row": {
+                                    'cells': [
+                                        [{'text': {'content': 'AUTOTEXT'}}],
+                                        [{'text': {'content': 'AUTOTEXT'}}],
+                                        [{'text': {'content': 'AUTOTEXT'}}],
+                                        [{'text': {'content': 'AUTOTEXT'}}],
+                                        [{'text': {'content': 'AUTOTEXT'}}],
+                                        [{'text': {'content': 'AUTOTEXT'}}]
+                                    ]
+                                }
+                            }
+                        ]
+                    }
+                }
+            ]
+        }
+        response = await self.__patch(method=method, data=data)
+        print(response)
+
+    async def update_table_row(self):
+        method = 'blocks/c6e6b21a-afac-454b-8b14-0d845fd4cf05/children'
+        data = {
+            'children': [
+                {
+                    "table_row": {
+                        'cells': [
+                            [{'text': {'content': 'AUTOTEXT'}}],
+                            [{'text': {'content': 'AUTOTEXT'}}],
+                            [{'text': {'content': 'AUTOTEXT'}}],
+                            [{'text': {'content': 'AUTOTEXT'}}]
+                        ]
+                    }
+                }
+            ]
+        }
+        response = await self.__patch(method=method, data=data)
+        print(response)
 
 
 async def main():
-    users = await Notion().get_users()
-    pprint(users)
+    db_id = 'a98b1f4047b84889a6e340fe482f4c7b'
+    res = await Notion().get_database(db_id)
+    # res = await Notion().database_query(db_id)
+    # res = await Notion().insert_into_db(db_id)
+    pprint(res)
 
 asyncio.run(main())
